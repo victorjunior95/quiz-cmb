64141613a6e67efecbbbe9b11afe684f2268ddc4
lucassouzs <lucasribeiro37@gmail.com>
2023-09-21 20:53:31 -0300

    front tela perguntas usuario

--- frontend/Scripts/Quiz_USR.js
+++ frontend/Scripts/Quiz_USR.js
@@ -26,26 +26,33 @@ function createDivQuestion(question, divAppend) {
   const buttonEnviar = document.createElement('button');
   
   textTema.textContent = tema;
+  textTema.className = 'textTema';
   textPergunta.textContent = pergunta;
   textPergunta.id = id;
+  textPergunta.className = 'textPergunta';
   imgPergunta.src = imagem;  
+  imgPergunta.className = 'imgPergunta';
   
   for(let index = 0; index < alternativas.length; index++) {
     const element = alternativas[index];
 
     const buttonAlternativa = document.createElement('button');
     buttonAlternativa.id = element.slice(0,1);
+    buttonAlternativa.className = 'buttonAlternativa';
     buttonAlternativa.textContent = element;
     buttonAlternativa.value = element.slice(0,1);
     buttonAlternativa.addEventListener('click', () => {
-      buttonAlternativa.style.backgroundColor = "green";
+      buttonAlternativa.style.backgroundColor = "#0AABBA";
+      buttonAlternativa.style.color = 'white';
       localStorage.setItem("alternativaSelecionada", buttonAlternativa.value);
     });
 
     divAlternativas.appendChild(buttonAlternativa);
+    divAlternativas.className = 'divAlternativas';
   }
 
   buttonEnviar.textContent = 'Confirmar';
+  buttonEnviar.className = 'buttonEnviar';
   buttonEnviar.type = 'submit';
   buttonEnviar.addEventListener('click', () => {
     const selectAnswer = localStorage.getItem("alternativaSelecionada");
