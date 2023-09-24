11ededdc7dfab72ffe8ee05a57af47740c4bc210
brenoxavier48 <brenoxavier8.bx@gmail.com>
2023-09-23 22:52:21 -0300

    feat: update difficulty

--- frontend/Scripts/Answer_APR.js
+++ frontend/Scripts/Answer_APR.js
@@ -1,41 +1,40 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = "http://localhost:3001";
 const socket = io(BASE_URL);
-let currentLevel = localStorage.getItem('actualLevel');
+let currentLevel = localStorage.getItem("actualLevel");
 const ROOMID = localStorage.getItem("roomId");
 
-
 const showAnswer = (question) => {
-  const { id, tema, pergunta, alternativas, imgResposta, resposta, descricao } = question;
+  const { id, tema, pergunta, alternativas, imgResposta, resposta, descricao } =
+    question;
 
-  const divAppend = document.getElementById('page_apr');
-  const divPergunta = document.createElement('div');
-  const divAlternativas = document.createElement('div');
-  const divImagem = document.createElement('div');
-  const textTema = document.createElement('h1');
-  const textPergunta = document.createElement('text');
-  const textDesc = document.createElement('text');
-  
+  const divAppend = document.getElementById("page_apr");
+  const divPergunta = document.createElement("div");
+  const divAlternativas = document.createElement("div");
+  const divImagem = document.createElement("div");
+  const textTema = document.createElement("h1");
+  const textPergunta = document.createElement("text");
+  const textDesc = document.createElement("text");
+
   textTema.textContent = tema;
-  textTema.className = 'textTema';
+  textTema.className = "textTema";
   textPergunta.textContent = pergunta;
-  textPergunta.className = 'textPergunta';
+  textPergunta.className = "textPergunta";
   textPergunta.id = id;
   textDesc.textContent = descricao;
-  textDesc.className = 'textDesc';
+  textDesc.className = "textDesc";
 
-  
-  for(let index = 0; index < alternativas.length; index++) {
+  for (let index = 0; index < alternativas.length; index++) {
     const element = alternativas[index];
     const isCorrect = element.slice(0, 1) === resposta;
 
-    const textAlternativa = document.createElement('li');
+    const textAlternativa = document.createElement("li");
     textAlternativa.id = element.slice(0, 1);
     textAlternativa.textContent = element;
-    textAlternativa.value = element.slice(0,1);
-    textAlternativa.className = 'textAlternativa';
-    textAlternativa.style.listStyleType = 'none';
-    
-    if(isCorrect) {
+    textAlternativa.value = element.slice(0, 1);
+    textAlternativa.className = "textAlternativa";
+    textAlternativa.style.listStyleType = "none";
+
+    if (isCorrect) {
       // Uma opção de estilização é aumentar a div da correta e diminuir as demais
       textAlternativa.style.backgroundColor = "green";
       textAlternativa.style.color = "white";
@@ -43,14 +42,14 @@ const showAnswer = (question) => {
 
     divAlternativas.appendChild(textAlternativa);
   }
-  
+
   divPergunta.appendChild(textTema);
   divPergunta.appendChild(textPergunta);
   divPergunta.appendChild(textDesc);
   // divAlternativas.appendChild(textDesc);
   if (imgResposta !== "") {
-    const imgElement = document.createElement('img');
-    imgElement.src = imgResposta;  
+    const imgElement = document.createElement("img");
+    imgElement.src = imgResposta;
     divImagem.appendChild(divAlternativas);
     divImagem.appendChild(imgElement);
     divPergunta.appendChild(imgElement);
@@ -59,45 +58,58 @@ const showAnswer = (question) => {
   divAlternativas.classList.add("divAlternativas");
   divAppend.appendChild(divPergunta);
   divAppend.appendChild(divAlternativas);
-}
+};
 
-socket.on('showAnswer', (question) => {
+socket.on("showAnswer", (question) => {
   document.getElementById("loading").remove();
   showAnswer(question);
-  socket.emit('sendQuestion', question, ROOMID);
+  socket.emit("sendQuestion", question, ROOMID);
 });
 
-socket.on('receiveTimer', () => {
+socket.on("receiveTimer", () => {
   totalTimer();
 });
 
 let nextButtonLink = "/pages/Loading_APR.html";
 
 const main = () => {
-  socket.emit('connectAPRAnswer', ROOMID);
+  socket.emit("connectAPRAnswer", ROOMID);
 
   const nextButton = document.getElementById("botaoAvancar");
-  nextButton.addEventListener('click', () => {
+  nextButton.addEventListener("click", () => {
     clearInterval(totalTimerInterval);
 
-    // O tempo da fase está reiniciando 
+    const completedAnswers = JSON.parse(
+      localStorage.getItem("perguntasCompletas")
+    );
+    const currentTime = JSON.parse(localStorage.getItem("currentTime"));
+    const isCurrentLevelCompleted = !completedAnswers[currentLevel]?.length;
+    console.log("completedAnswers", completedAnswers);
+    console.log("isCurrentLevelCompleted", isCurrentLevelCompleted);
+    console.log("currentTime", currentTime);
+    // O tempo da fase está reiniciando
 
     // Posso passar essa lógica para o if no final do script (?)
-    if (document.getElementById('counter').getAttribute('aria-timer') <= 0) {
-      localStorage.setItem(`${currentLevel}`, 'acabou');
+    if (currentTime.time <= 0 || isCurrentLevelCompleted) {
+      console.log("entrou");
+      localStorage.setItem(`${currentLevel}`, "acabou");
 
-      let newLevel = currentLevel === 'facil' ? 'media' : 'dificil';
+      let newLevel = currentLevel === "facil" ? "media" : "dificil";
 
-      localStorage.setItem('actualLevel', newLevel);
+      localStorage.setItem("actualLevel", newLevel);
+      localStorage.setItem(
+        "currentTime",
+        JSON.stringify({ started: false, time: 0 })
+      );
     }
 
     window.location.href = nextButtonLink;
   });
 
   const classificationButton = document.getElementById("botaoClassificacao");
-  classificationButton.addEventListener('click', () => {
+  classificationButton.addEventListener("click", () => {
     clearInterval(totalTimerInterval);
-    console.log('AnswerClass');
+    console.log("AnswerClass");
     window.location.href = "/pages/Classification_APR.html";
   });
 };
@@ -109,24 +121,26 @@ let totalTimerInterval;
 function totalTimer() {
   const counter = document.getElementById("counter");
 
-  const localStorageTime = JSON.parse(localStorage.getItem('currentTime'));
+  const localStorageTime = JSON.parse(localStorage.getItem("currentTime"));
   const currentTime = localStorageTime.time;
 
   var timeInMilliseconds = currentTime;
 
-    // Função para buscar onde time foi pausado
-    function showsPausedTime() {
-      var minutesRemaining = Math.floor(timeInMilliseconds / 60000);
-      var secondsRemaining = Math.floor((timeInMilliseconds % 60000) / 1000);
+  // Função para buscar onde time foi pausado
+  function showsPausedTime() {
+    var minutesRemaining = Math.floor(timeInMilliseconds / 60000);
+    var secondsRemaining = Math.floor((timeInMilliseconds % 60000) / 1000);
 
-      // Formate os minutos e segundos para exibição
-      var formattedMinutes = minutesRemaining < 10 ? "0" + minutesRemaining : minutesRemaining;
-      var formattedSeconds = secondsRemaining < 10 ? "0" + secondsRemaining : secondsRemaining;
+    // Formate os minutos e segundos para exibição
+    var formattedMinutes =
+      minutesRemaining < 10 ? "0" + minutesRemaining : minutesRemaining;
+    var formattedSeconds =
+      secondsRemaining < 10 ? "0" + secondsRemaining : secondsRemaining;
 
-      // Exibe tempo pausado na tela
-      counter.innerHTML = formattedMinutes + ":" + formattedSeconds;
-    }
+    // Exibe tempo pausado na tela
+    counter.innerHTML = formattedMinutes + ":" + formattedSeconds;
+  }
 
-    // Inicializa função
-    showsPausedTime();
-};
+  // Inicializa função
+  showsPausedTime();
+}
